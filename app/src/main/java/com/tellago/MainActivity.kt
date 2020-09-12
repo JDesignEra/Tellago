d208a9c078fbcaf5cf343189abe1a5c163caaf1f
Daryl <t.daryl10196@gmail.com>
2020-09-12 10:56:06 +0800

    Implemented toolbar actions within a Navigation Drawer (override navBtn)

--- app/src/main/java/com/tellago/MainActivity.kt
+++ app/src/main/java/com/tellago/MainActivity.kt
@@ -2,23 +2,30 @@ package com.tellago
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
 import android.os.Handler
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.firebase.ui.auth.AuthMethodPickerLayout
 import com.firebase.ui.auth.AuthUI
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.tellago.fragments.*
 import com.tellago.services.AuthExitService
 import com.tellago.utils.CustomToast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.menu_header.*
+import kotlinx.android.synthetic.main.toolbar_drawer.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -46,8 +53,12 @@ class MainActivity : AppCompatActivity() {
 
         user = FirebaseAuth.getInstance().currentUser
 
+        // To hide bottom navigation between sign out & login
+//        bottom_navigation.visibility = View.INVISIBLE
+//        guest_bot_banner.visibility = View.INVISIBLE
+
         // Running is custom layout for landing page (functionality handled by FirebaseUI)
-        val customLayout : AuthMethodPickerLayout = AuthMethodPickerLayout
+        val customLayout: AuthMethodPickerLayout = AuthMethodPickerLayout
             .Builder(R.layout.activity_auth)
             .setEmailButtonId(R.id.btnEmail)
             .setFacebookButtonId(R.id.btnFacebook)
@@ -70,7 +81,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
-
         if (hasFocus) hideSystemUI()
     }
 
@@ -81,13 +91,21 @@ class MainActivity : AppCompatActivity() {
             if (resultCode == Activity.RESULT_OK) {
                 user = FirebaseAuth.getInstance().currentUser
 
+                if (user != null && user!!.isAnonymous) {
+                    bottom_navigation.visibility = View.INVISIBLE
+                    guest_bot_banner.visibility = View.VISIBLE
+                } else {
+                    bottom_navigation.visibility = View.VISIBLE
+                    guest_bot_banner.visibility = View.INVISIBLE
+                }
+
                 addFragment(homeFragment)
 
                 if (user != null && user!!.isAnonymous) {
                     CustomToast(this, "Welcome to Tellago, Guest").success()
-                }
-                else {
+                } else {
                     CustomToast(this, "Welcome to Tellago, %s".format(user?.displayName)).success()
+                    user_displayname.text = "Greetings, %s".format(user?.displayName)
                 }
             }
         }
@@ -110,6 +128,11 @@ class MainActivity : AppCompatActivity() {
         // Indicate that toolbar will replace Actionbar
         setSupportActionBar(toolbar as Toolbar?)
 
+
+        configureNavigationDrawer()
+        configureToolbar()
+        navigation.visibility = View.INVISIBLE
+
         StartTimer()
 
         if (user != null) {
@@ -123,7 +146,7 @@ class MainActivity : AppCompatActivity() {
         // the following code will replace the current fragment based on the selected navigation
         // item from the bottom navigation bar
         bottom_navigation.setOnNavigationItemSelectedListener {
-            when(it.itemId){
+            when (it.itemId) {
                 R.id.ic_home -> replaceFragment(homeFragment)
                 R.id.ic_people -> replaceFragment(communityFragment)
                 R.id.ic_flag -> replaceFragment(lifeAspirationFragment)
@@ -135,34 +158,20 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val inflater : MenuInflater = menuInflater
-        inflater.inflate(R.menu.options_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.item_logout -> signOut()
-        }
-        return true
-    }
-
-    private fun addFragment(fragment: Fragment){
+    private fun addFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment_container, fragment)
         transaction.addToBackStack(null);
         transaction.commit()
     }
 
-    private fun replaceFragment(fragment: Fragment){
-        if(fragment != null){
+    private fun replaceFragment(fragment: Fragment) {
+        if (fragment != null) {
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, fragment)
             transaction.addToBackStack(null);
             transaction.commit()
-        }
-        else {
+        } else {
             addFragment(fragment)
         }
     }
@@ -180,6 +189,64 @@ class MainActivity : AppCompatActivity() {
 
                 startActivity(intent)
             }
+    }
+
+    private fun configureToolbar() {
+        setSupportActionBar(toolbar as Toolbar?)
+        val actionbar: ActionBar? = supportActionBar
+        // To 'hide' Title display in actionbar
+        actionbar?.setTitle("")
+
+        // Open up navigation in the event that 'Menu icon' is clicked (it is actually Navigation button)
+        (toolbar as Toolbar?)?.setNavigationOnClickListener {
+            navigation.visibility = View.VISIBLE
+        }
+
+        // Set 'Menu icon' as the icon for Navigation button
+        actionbar?.setHomeAsUpIndicator(R.drawable.ic_menu)
+        actionbar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun configureNavigationDrawer() {
+        val navigationView: NavigationView = navigation
+        navigationView.setNavigationItemSelectedListener {
+            onNavigationItemSelected(it)
+            true
+        }
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        val viewRect = Rect()
+        navigation.getGlobalVisibleRect(viewRect)
+        // if user taps outside of toolbar navigation drawer, then change its visibility to INVISIBLE
+        if (!viewRect.contains(ev!!.rawX.toInt(), ev.rawY.toInt())) {
+            navigation.visibility = View.INVISIBLE
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    fun onNavigationItemSelected(menuItem: MenuItem) {
+        val f: Fragment? = null
+        val menu_itemID = menuItem.itemId
+
+        when (menu_itemID) {
+            R.id.logout_from_drawer -> signOut()
+        }
+
+        if (f != null) {
+            val drawerLayout: DrawerLayout = drawer_layout
+            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.toolbar_frame, f)
+            transaction.commit()
+            drawerLayout.closeDrawers()
+            true
+        } else
+            false
+    }
+
+    override fun onMenuOpened(featureId: Int, menu: Menu): Boolean {
+        navigation.visibility = View.VISIBLE
+        return super.onMenuOpened(featureId, menu)
     }
 
     private fun hideSystemUI() {
--- app/src/main/java/com/tellago/fragments/HomeFragment.kt
+++ app/src/main/java/com/tellago/fragments/HomeFragment.kt
@@ -1,13 +1,18 @@
 package com.tellago.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.navigation.NavigationView
 import com.tellago.MainActivity
 import com.tellago.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.toolbar_drawer.*
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -21,6 +26,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         if (MainActivity.user != null && !MainActivity.user!!.isAnonymous) {
             // User is signed in
             greeting.text = "Welcome to Tellago, %s".format(MainActivity.user!!.displayName)
@@ -31,4 +37,6 @@ class HomeFragment : Fragment() {
             greeting.text = greetingText
         }
     }
+
+
 }
