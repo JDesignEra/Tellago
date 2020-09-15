cd28bba75e5fd3b375f18257ad3d7034056426b6
JDesign <tgm.joel@gmail.com>
2020-09-15 21:32:30 +0800

    Cleaned up HomeFragments.kt code

--- app/src/main/java/com/tellago/fragments/HomeFragment.kt
+++ app/src/main/java/com/tellago/fragments/HomeFragment.kt
@@ -3,18 +3,17 @@ package com.tellago.fragments
 import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import androidx.fragment.app.Fragment
-import androidx.appcompat.app.ActionBar
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.FragmentTransaction
-import com.google.android.material.navigation.NavigationView
-import com.tellago.MainActivity
+import androidx.fragment.app.Fragment
 import com.tellago.R
 import com.tellago.models.Auth
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -22,10 +21,8 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -41,9 +38,7 @@ class HomeFragment : Fragment() {
         }
 
         configureToolbar()
-
     }
-
 
     private fun configureToolbar() {
         toolbar_Home.setNavigationOnClickListener {
@@ -58,7 +53,6 @@ class HomeFragment : Fragment() {
 
             // Tie DrawerLayout events to ActionBarToggle
             drawerLayout.addDrawerListener(drawerToggle)
-
         }
     }
 
@@ -66,7 +60,6 @@ class HomeFragment : Fragment() {
         val drawerLayout : DrawerLayout = activity?.drawer_layout ?: DrawerLayout(this.requireContext())
         return ActionBarDrawerToggle(this.requireActivity(), drawerLayout, toolbar_Home, R.string.drawer_open, R.string.drawer_close)
     }
-
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         super.onConfigurationChanged(newConfig)
