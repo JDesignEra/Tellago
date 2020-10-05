1dadd5d49aecdefe8c406f64196623796f65dfb3
Daryl <t.daryl10196@gmail.com>
2020-10-05 16:22:57 +0800

    Mini update for ShowBucketListItemsFragment
    - swiping left and right now possible
    - delete functionality for LEFT swipe
    - undo functionality not working (after delete)
    - archive functionality not complete

--- app/src/main/java/com/tellago/adapters/ShowBucketListItemsRecyclerAdapter.kt
+++ app/src/main/java/com/tellago/adapters/ShowBucketListItemsRecyclerAdapter.kt
@@ -7,22 +7,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.tellago.R
 import com.tellago.fragments.EditBucketListItemFragment
 import com.tellago.models.BucketListItem
 import com.tellago.models.Goal
+import com.tellago.utilities.CustomToast
 import com.tellago.utilities.FragmentUtils
+import kotlinx.android.synthetic.main.fragment_show_bucket_list_items.view.*
 import kotlinx.android.synthetic.main.layout_bucket_list_item.view.*
 
 
 class ShowBucketListItemsRecyclerAdapter(private var itemNames: List<String>) :
     RecyclerView.Adapter<ShowBucketListItemsRecyclerAdapter.BucketListItemViewHolder>() {
 
+    var listOfBucketItems = mutableListOf<String>()
+
+    init {
+        this.listOfBucketItems = itemNames as MutableList<String>
+    }
 
     private var bundle: Bundle? = null
+    private var deletedItemName: String = ""
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -50,10 +61,31 @@ class ShowBucketListItemsRecyclerAdapter(private var itemNames: List<String>) :
     }
 
 
+    // function to remove/delete a bucket list item (given its position/index)
+    fun delete(position : Int) {
+        listOfBucketItems.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    // function to add a bucket list item (given its position/index)
+    fun add(position : Int, string : String) {
+        listOfBucketItems.add(position, string)
+        Log.d("new item added", "FIRED")
+        notifyDataSetChanged()
+    }
+
+    // function to add a bucket list item (given its position/index)
+    fun retrieve(position : Int) {
+        listOfBucketItems[position]
+        notifyDataSetChanged()
+    }
+
+
     class BucketListItemViewHolder constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         //var model: BucketListItem? = null
         val blItemName: TextView = itemView.tv_bucketList_Title
         var goal = Goal()
+
 
 //        if (this.arguments != null) bundle = requireArguments()
 //        if (bundle != null) goal = bundle!!.getParcelable(goal::class.java.name)!!
@@ -61,6 +93,7 @@ class ShowBucketListItemsRecyclerAdapter(private var itemNames: List<String>) :
 
         init {
             val activity: AppCompatActivity = itemView.context as AppCompatActivity
+
 
             itemView.btn_ShowBucketItemDetails.setOnClickListener { v: View? ->
                 val position: Int = adapterPosition
@@ -82,12 +115,16 @@ class ShowBucketListItemsRecyclerAdapter(private var itemNames: List<String>) :
 
             }
 
+
             itemView.btn_CompleteBucketItem.setOnClickListener {
                 Log.d("Completed Bucket Item", "FIRED")
             }
 
         }
+
+
     }
+
 
 
 }
--- app/src/main/java/com/tellago/fragments/ShowBucketListItemsFragment.kt
+++ app/src/main/java/com/tellago/fragments/ShowBucketListItemsFragment.kt
@@ -6,16 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.gms.tasks.Task
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.FirebaseFirestore
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.tellago.R
 import com.tellago.adapters.ShowBucketListItemsRecyclerAdapter
-import com.tellago.models.Auth
-import com.tellago.models.BucketListItem
 import com.tellago.models.Goal
 import com.tellago.utilities.FragmentUtils
+import com.tellago.utilities.SwipeToDelete
 import kotlinx.android.synthetic.main.fragment_show_bucket_list_items.*
 
 
@@ -23,11 +22,13 @@ class ShowBucketListItemsFragment : Fragment() {
     private lateinit var fragmentUtils: FragmentUtils
     private var bundle: Bundle? = null
     private lateinit var goal: Goal
+
     private var adapter: ShowBucketListItemsRecyclerAdapter? = null
     private val createBucketListItemFragment: Fragment = CreateBucketListItemFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         goal = Goal()
         if (this.arguments != null) bundle = requireArguments()
@@ -40,6 +41,7 @@ class ShowBucketListItemsFragment : Fragment() {
 
         // Send correct ArrayList to ShowBucketListItemsRecyclerAdapter
         adapter = ShowBucketListItemsRecyclerAdapter(goal.bucketList)
+
 
     }
 
@@ -61,6 +63,65 @@ class ShowBucketListItemsFragment : Fragment() {
         )
         recycler_view_show_bucketListItems_fragment.adapter = adapter
 
+
+
+        val item = object : SwipeToDelete(
+            activity?.application?.baseContext,
+            0,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                Log.d("onSwiped", "FIRED")
+
+                if (!goal.gid?.isBlank()!!) {
+                    goal.setByGid {
+                        if (it != null) {
+                            Log.d("goal id saving", goal.gid.toString())
+                        }
+                    }
+                }
+
+                Log.d("savedRecords", "FIRED")
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    // if swiped LEFT (from right of screen to left of screen) --> display snackbar
+                    val deletedItemName = adapter!!.retrieve(viewHolder.adapterPosition).toString()
+                    Snackbar.make(
+                        viewHolder.itemView,
+                        "Deleted bucket list item #{${viewHolder.adapterPosition})}",
+                        Snackbar.LENGTH_SHORT
+                    )
+                        .setAction("Undo") {
+                            it.setOnClickListener {
+                                adapter!!.add(viewHolder.adapterPosition, deletedItemName)
+                                adapter!!.notifyItemInserted(viewHolder.adapterPosition)
+                                Log.d("Undo Delete", "FIRED")
+                            }
+                        }
+                        .show()
+                    adapter!!.delete(viewHolder.adapterPosition + 1)
+
+                }
+                else if (direction == ItemTouchHelper.RIGHT) {
+                    // if swiped RIGHT (from left of screen to right of screen) --> display snackbar
+                    Snackbar.make(
+                        viewHolder.itemView,
+                        "Archived bucket list item #{${viewHolder.adapterPosition + 1})}",
+                        Snackbar.LENGTH_SHORT
+                    // missing functionality to archive. I.e. change status attribute of BucketListItem object
+
+                    )
+                        .show()
+                }
+
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(item)
+        // Attach ItemTouchHelper to recycler_view_show_bucketListItems_fragment
+        itemTouchHelper.attachToRecyclerView(recycler_view_show_bucketListItems_fragment)
+
+
         fab_add_bucketListItem.setOnClickListener {
             Log.d("FAB Add BL item", "FIRED")
             //fragmentUtils.replace(createBucketListItemFragment)
@@ -75,7 +136,10 @@ class ShowBucketListItemsFragment : Fragment() {
             ).replace(createBucketListItemFragment)
 
         }
+
+
     }
+
 
     private fun configureToolbar() {
         toolbar_show_bucketListItems.setNavigationIcon(R.drawable.ic_arrow_back_36)
