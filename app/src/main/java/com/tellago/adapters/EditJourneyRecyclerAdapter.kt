0c8ee4e3b7be8e54c73caaf488170ae9a60ef1ce
JDesign <tgm.joel@gmail.com>
2020-10-26 13:24:10 +0800

    Journey Edit/Add WIP

---
+++ app/src/main/java/com/tellago/adapters/EditJourneyRecyclerAdapter.kt
@@ -0,0 +1,11 @@
+package com.tellago.adapters
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.tellago.models.Journey
+
+// TODO
+//class EditJourneyRecyclerAdapter(options: FirestoreRecyclerOptions<Journey>)
+//    : FirestoreRecyclerAdapter<Journey, ShowGoalsRecyclerAdapter.GoalViewHolder>(options) {
+//
+//}
---
+++ app/src/main/java/com/tellago/fragments/EditJourneyFragment.kt
@@ -0,0 +1,18 @@
+package com.tellago.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tellago.R
+
+class EditJourneyFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_edit_journey, container, false)
+    }
+}
